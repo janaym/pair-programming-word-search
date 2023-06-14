731ba8f8edd382e715bf17e5f126d2ae4efacb83
Janay Ma <janay.ma7@gmail.com>
2023-06-14 10:27:47 -0700

    implement vertical search and test for empty array

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,26 @@
+const transpose = require('../../week4/async/matrix_transposition');
+
 const wordSearch = (letters, word) => { 
+    if(letters.length === 0) {
+        return false;
+    }
+
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word)) {
+            return true
+        }
     }
+    let verticalJoin = transpose(letters).map(ls => ls.join(''));
+    for (l of verticalJoin) {
+        if (l.includes(word)) {
+            return true
+        }
+    }
+
+    return false;
 }
 
 module.exports = wordSearch
+
+//console.log(transpose([[1,0],[2,3]]));
